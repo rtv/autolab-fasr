291d9daa26e2281c360782365cc38fc6c5bfea67
Jens <jwawerla@sfu.ca>
2009-05-15 10:02:07 -0700

    FASR moved to lib nd, wp and rapi

--- build_dependencies.sh
+++ build_dependencies.sh
@@ -4,42 +4,44 @@ PROJECT_DIR=`pwd`
 DEPENDENS_SRC_DIR=$PROJECT_DIR/local/src
 DEPENDENS_INSTALL_DIR=$PROJECT_DIR/local
 
-PKG_CONFIG_PATH_ORG=$PKG_CONFIG_PATH
+RED='\e[1;31m'
+EC='\033[m'
+
 export PKG_CONFIG_PATH=$PKG_CONFIG_PATH:$DEPENDENS_INSTALL_DIR/lib/pkgconfig
 
-echo changing PKG_CONFIG_PATH to $PKG_CONFIG_PATH
+echo -e ${RED}changing PKG_CONFIG_PATH to ${EC} ${PKG_CONFIG_PATH}
 
-echo creating install directory $DEPENDENCE_SRC_DIR
+echo -e ${RED}creating install directory ${EC} $DEPENDENCE_SRC_DIR
 mkdir -p ${DEPENDENS_SRC_DIR}
 
 ######################################
 # get ND
-echo Fetching autolab-nd
+echo -e ${RED}Fetching autolab-nd ${EC}
 cd ${DEPENDENS_SRC_DIR}
 git clone git@github.com:rtv/autolab-nd.git
 cd ${DEPENDENS_SRC_DIR}/autolab-nd
 
-echo Reverting autolab-nd to the correct commit version
+echo -e ${RED}Reverting autolab-nd to the correct commit version ${EC}
 #git reset --hard a80def672b1cf887ba797bd5f43b3abd8ae600af
 
 ######################################
 # get WP
-echo Fetching autolab-wp
+echo -e ${RED}Fetching autolab-wp ${EC}
 cd ${DEPENDENS_SRC_DIR}
 git clone git@github.com:rtv/autolab-wp.git
 cd ${DEPENDENS_SRC_DIR}/autolab-wp
 
-echo Reverting autolab-wp to the correct commit version
+echo -e ${RED}Reverting autolab-wp to the correct commit version ${EC}
 #git reset --hard 14cf4e2c570ac5ff15bd487f67357583505c51b8
 
 ######################################
 # get RAPI
-echo Fetching autolab-rapi
+echo -e ${RED}Fetching autolab-rapi ${EC}
 cd ${DEPENDENS_SRC_DIR}
 git clone git@github.com:jwawerla/autolab-rapi.git
 cd ${DEPENDENS_SRC_DIR}/autolab-rapi
 
-echo Reverting autolab-rapi to the correct commit version
+echo -e ${RED}Reverting autolab-rapi to the correct commit version ${EC}
 #git reset --hard 14cf4e2c570ac5ff15bd487f67357583505c51b8
 
 
@@ -47,18 +49,21 @@ echo Reverting autolab-rapi to the correct commit version
 
 ######################################
 # Install rapi
+echo -e ${RED}Installing autolab-rapi ${EC}
 cd ${DEPENDENS_SRC_DIR}/autolab-rapi
 cmake -DCMAKE_INSTALL_PREFIX=${DEPENDENS_INSTALL_DIR}
 make install
 
 ######################################
 # Install nd
+echo -e ${RED}Installing autolab-nd ${EC}
 cd ${DEPENDENS_SRC_DIR}/autolab-nd
 cmake -DCMAKE_INSTALL_PREFIX=${DEPENDENS_INSTALL_DIR} -DPKG_CONFIG_PATH=$DEPENDENS_INSTALL_DIR/lib/pkconfig
 make install
 
 ######################################
 # Install wp
+echo -e ${RED}Installing autolab-wp ${EC}
 cd ${DEPENDENS_SRC_DIR}/autolab-wp
 cmake -DCMAKE_INSTALL_PREFIX=${DEPENDENS_INSTALL_DIR}
 make install
@@ -66,9 +71,6 @@ make install
 
 ######################################
 # Finishing up
-
-# restore PKG_CONFIG_PATH
-#export PKG_CONFIG_PATH=$PKG_CONFIG_PATH_ORG
 
 # change back to project dir and we are done
 cd $PROJECT_DIR
